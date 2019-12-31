8beb680e8944d3d2406599b5f5a17a030bfbc18d
Alyssa Bonesteel <anbonesteel@gmail.com>
2019-12-31 12:41:40 -0700

    first push

---
+++ src/CartTotal.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import slugify from 'slugify';
+
+const CartTotal = props => {
+    const summary = Object.keys(props.selected).map((feature, idx) => {
+        const featureHash = feature + '-' + idx;
+        const selectedOption = props.selected[feature];
+
+        return (
+        <div className="summary__option" key={featureHash}>
+            <div className="summary__option__label">{feature} </div>
+            <div className="summary__option__value">{selectedOption.name}</div>
+            <div className="summary__option__cost">
+            {props.USCurrencyFormat.format(selectedOption.cost)}
+            </div>
+        </div>
+        );
+    });
+
+    const total = Object.keys(props.selected).reduce(
+    (acc, curr) => acc + props.selected[curr].cost,
+    0
+    );
+return summary;
+
+};
+ 
+
+ export default CartTotal;
---
+++ src/CustomizerOptions.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import slugify from 'slugify';
+
+
+const CustomizerOptions = props => {
+    const features = Object.keys(props.features).map((feature, idx) => {
+        const featureHash = feature + '-' + idx;
+        const options = props.features[feature].map(item => {
+          const itemHash = slugify(JSON.stringify(item));
+          return (
+            <div key={itemHash} className="feature__item">
+              <input
+                type="radio"
+                id={itemHash}
+                className="feature__option"
+                name={slugify(feature)}
+                checked={item.name === props.selected[feature].name}
+                onChange={e => props.updateFeature(feature, item)}
+              />
+              <label htmlFor={itemHash} className="feature__label">
+                {item.name} ({props.USCurrencyFormat.format(item.cost)})
+              </label>
+            </div>
+          );
+        });
+        // render this inside customize component 
+        return (
+          <fieldset className="feature" key={featureHash}>
+            <legend className="feature__name">
+              <h3>{feature}</h3>
+            </legend>
+            {options}
+          </fieldset>
+        );
+      });
+return features;
+};
+
+export default CustomizerOptions;
+
